bbba557cbe3dd75e38e2cbaec96747a2fb8ed26d
galborovoi <gal.borovoi@huji.mail.ac.il>
2024-10-29 11:17:42 +0200

    Changed relic size

--- Assets/Scripts/Managers/Relic Manager.cs
+++ Assets/Scripts/Managers/Relic Manager.cs
@@ -4,19 +4,20 @@ using UnityEngine;
 
 public class RelicManager : Singleton<RelicManager>
 {
+    private float RELICIMAGESIZE = 100f; //it's 100x100
+    
     [SerializeField] private Transform _relicParent;
     [SerializeField] private float _spacing;
 
     private List<AbstractRelic> _relics;
 
     protected RelicManager()
-    {
-        
-    }
+    { }
 
     private void Awake()
     {
         _relics = new List<AbstractRelic>();
+        _spacing *= RELICIMAGESIZE;
     }
 
     public void AddRelic(AbstractRelic relicPrefab)
