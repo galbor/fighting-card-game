3f515f164151eacf5a407b170f3b5052145b24cf
galborovoi <gal.borovoi@huji.mail.ac.il>
2024-11-19 15:34:07 +0200

    Added map changes

--- Assets/Scripts/Managers/Map Manager.cs
+++ Assets/Scripts/Managers/Map Manager.cs
@@ -5,12 +5,22 @@ namespace Managers
     public class MapManager : Singleton<MapManager>
     {
         protected MapManager() { }
-
-        [SerializeField] private GameObject _map;
-
+        [SerializeField] private GameObject[] _possibleMaps;
+        
+        private GameObject _map;
+        private int _currentMap = 0;
+        
         public void SetMapActive(bool active)
         {
             _map.SetActive(active);
+        }
+
+        public void SetNextMap()
+        {
+            if (_currentMap > 0) Destroy(_map);
+            _map = Instantiate(_possibleMaps[_currentMap]);
+            _currentMap++;
+            SetMapActive(true);
         }
     }
 }
