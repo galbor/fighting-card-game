bd8860d6c7a7e51cea3188b1744062c3d4de10f9
galborovoi <gal.borovoi@huji.mail.ac.il>
2024-11-27 12:52:47 +0200

    Added card that adds status effect

---
+++ Assets/Scripts/Cards/GainStatusEffectCard.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using DefaultNamespace.StatusEffects;
+using DefaultNamespace.Utility;
+using UnityEngine;
+
+namespace cards
+{
+    [CreateAssetMenu(menuName = "Cards/Gain Status Effect Card")]
+    public class GainStatusEffectCard : BasicCard
+    {
+        [SerializeField] private BodyPartStatusEffect.StatusType _statusEffect;
+        [SerializeField] private int _amt;
+
+        private string GetStatusName()
+        {
+            return _statusEffect.ToString();
+        }
+        
+        public override void Play(Person user, List<Person.BodyPartEnum> attacking_parts, Person target,
+            Person.BodyPartEnum affected_part)
+        {
+            base.Play(user, attacking_parts, target, affected_part);
+            
+            attacking_parts.ForEach(x => user.GetHealthBar(x).AddStatusEffect(_statusEffect, _amt));
+        }
+        
+        protected override void UpdateDescription()
+        {
+            base.UpdateDescription();
+            _displayDescription = MyUtils.ReplaceAllBrackets(_displayDescription, "status", GetStatusName());
+            _displayDescription = MyUtils.ReplaceAllBrackets(_displayDescription, "amount", _amt.ToString());
+        }
+
+        protected override string GenerateThisDescription()
+        {
+            var res = new StringBuilder(base.GenerateThisDescription());
+            foreach (var attackerType in AttackerType)
+            {
+                res.AppendFormat("Gain {0} stacks of {1} on your {2}.\n",
+                    _amt , GetStatusName(), attackerType.ToString());
+            }
+
+            return res.ToString();
+        }
+    }
+}
