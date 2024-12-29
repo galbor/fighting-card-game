311a3391399f34fdc66b731134aed4cd875d3eba
galborovoi <gal.borovoi@huji.mail.ac.il>
2024-12-29 20:34:16 +0200

    Added ApplyStatusEffect card (now GainStatusEffect inherits it)

---
+++ Assets/Scripts/Cards/ApplyStatusEffect.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using cards;
+using DefaultNamespace.StatusEffects;
+using DefaultNamespace.UI;
+using DefaultNamespace.Utility;
+using UnityEditor;
+using UnityEngine;
+
+namespace cards
+{
+    [CreateAssetMenu(menuName = "Cards/Apply Status Effect Card")]
+    public class ApplyStatusEffect : BasicCard
+    {
+        [SerializeField] protected BodyPartStatusEffect.StatusType _statusEffect;
+        [SerializeField] protected int _amt;
+
+        protected string _defaultDescriptionFormat = "Apply {0} stacks of {1} on the enemy's {2}.\n";
+
+        public override void Play(Person user, List<Person.BodyPartEnum> attacking_parts, Person target,
+            Person.BodyPartEnum affected_part)
+        {
+            base.Play(user, attacking_parts, target, affected_part);
+
+            attacking_parts.ForEach(x => AddStatusEffect(user, user.GetHealthBar(x), target, affected_part));
+        }
+
+        protected virtual void AddStatusEffect(Person user, HealthBar attacking_part, Person target,
+            Person.BodyPartEnum affected_part)
+        {
+            target.GetHealthBar(affected_part).AddStatusEffect(BodyPartStatusEffect.GetTypeOfStatusType(_statusEffect), _amt);
+            
+        }
+
+        protected string GetStatusName()
+        {
+            return _statusEffect.ToString();
+        }
+
+        protected override string GenerateThisDescription()
+        {
+            var res = new StringBuilder(base.GenerateThisDescription());
+            foreach (var attackerType in AttackerType)
+            {
+                res.Append(FormattedSingleAttackerTypeDescription(attackerType));
+            }
+
+            return res.ToString();
+        }
+        
+        /**
+         * formats a single AttackerType's description
+         */
+        protected virtual string FormattedSingleAttackerTypeDescription(AttackerTypeEnum attackerType)
+        {
+            return string.Format(_defaultDescriptionFormat,
+                    _amt, GetStatusName(), TargetType.ToString());
+        }
+
+        protected override void UpdateDescription()
+        {
+            base.UpdateDescription();
+            _displayDescription = MyUtils.ReplaceAllBrackets(_displayDescription, "status", GetStatusName());
+            _displayDescription = MyUtils.ReplaceAllBrackets(_displayDescription, "amount", _amt.ToString());
+        }
+    }
+}
--- Assets/Scripts/Cards/GainStatusEffectCard.cs
+++ Assets/Scripts/Cards/GainStatusEffectCard.cs
@@ -1,47 +1,33 @@
 ﻿using System.Collections.Generic;
 using System.Text;
 using DefaultNamespace.StatusEffects;
+using DefaultNamespace.UI;
 using DefaultNamespace.Utility;
 using UnityEngine;
 
 namespace cards
 {
     [CreateAssetMenu(menuName = "Cards/Gain Status Effect Card")]
-    public class GainStatusEffectCard : BasicCard
+    public class GainStatusEffectCard : ApplyStatusEffect
     {
-        [SerializeField] private BodyPartStatusEffect.StatusType _statusEffect;
-        [SerializeField] private int _amt;
 
-        private string GetStatusName()
+        protected GainStatusEffectCard()
         {
-            return _statusEffect.ToString();
+            _defaultDescriptionFormat = "Gain {0} stacks of {1} on your {2}.\n";
         }
-        
-        public override void Play(Person user, List<Person.BodyPartEnum> attacking_parts, Person target,
+
+        protected override void AddStatusEffect(Person user, HealthBar attacking_part, Person target,
             Person.BodyPartEnum affected_part)
         {
-            base.Play(user, attacking_parts, target, affected_part);
-            
-            attacking_parts.ForEach(x => user.GetHealthBar(x).AddStatusEffect(BodyPartStatusEffect.GetTypeOfStatusType(_statusEffect), _amt));
-        }
-        
-        protected override void UpdateDescription()
-        {
-            base.UpdateDescription();
-            _displayDescription = MyUtils.ReplaceAllBrackets(_displayDescription, "status", GetStatusName());
-            _displayDescription = MyUtils.ReplaceAllBrackets(_displayDescription, "amount", _amt.ToString());
+            attacking_part.AddStatusEffect(BodyPartStatusEffect.GetTypeOfStatusType(_statusEffect), _amt);
         }
 
-        protected override string GenerateThisDescription()
+        protected override string FormattedSingleAttackerTypeDescription(AttackerTypeEnum attackerType)
         {
-            var res = new StringBuilder(base.GenerateThisDescription());
-            foreach (var attackerType in AttackerType)
-            {
-                res.AppendFormat("Gain {0} stacks of {1} on your {2}.\n",
-                    _amt , GetStatusName(), attackerType.ToString());
-            }
-
-            return res.ToString();
+            return string.Format(_defaultDescriptionFormat,
+                    _amt, GetStatusName(), attackerType.ToString());
         }
+
+
     }
 }
