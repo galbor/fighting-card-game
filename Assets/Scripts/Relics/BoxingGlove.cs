0956e76dbc1d75c28ff190878da483b79af87609
galborovoi <gal.borovoi@huji.mail.ac.il>
2025-05-20 11:46:31 +0300

    Added Boxing Glove relic as Boxer starting relic

---
+++ Assets/Scripts/Relics/BoxingGlove.cs
@@ -0,0 +1,33 @@
+﻿using Assets.Scripts.Status_Effects;
+using DefaultNamespace.Utility;
+using Managers;
+using UnityEngine;
+
+namespace Relics
+{
+    class BoxingGlove : AbstractRelic
+    {
+        [SerializeField] private int _strength;
+
+        protected new void Awake()
+        {
+            base.Awake();
+
+            EventManager.Instance.StartListening(EventManager.EVENT__START_COMBAT, Strengthen);
+
+            Description = MyUtils.ReplaceAllBrackets(Description, "strength", _strength.ToString());
+        }
+
+        /**
+         * gives the knife status effect, which inflicts bleed when attacking
+         */
+        private void Strengthen(object obj)
+        {
+            Player.Instance.Person.GetHealthBar(Person.BodyPartEnum.LEFT_ARM).AddStatusEffect(typeof(StrengthStatusEffect), _strength);
+            Player.Instance.Person.GetHealthBar(Person.BodyPartEnum.RIGHT_ARM).AddStatusEffect(typeof(StrengthStatusEffect), _strength);
+            Player.Instance.Person.GetHealthBar(Person.BodyPartEnum.LEFT_LEG).AddStatusEffect(typeof(StrengthStatusEffect), -_strength);
+            Player.Instance.Person.GetHealthBar(Person.BodyPartEnum.RIGHT_LEG).AddStatusEffect(typeof(StrengthStatusEffect), -_strength);
+            Activate();
+        }
+    }
+}
